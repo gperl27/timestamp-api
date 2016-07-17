8babc0f4ca16e90eff46b98b115a612ac30f50de
Greg Perlman <gperlman27@gmail.com>
2016-07-17 15:19:05 +0000

    finished timestamp

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
 var express = require('express');
 var app = express();
 
+var moment = require('moment');
+var port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/views'));
+
 app.get('/', function(req,res){
-    res.send('Hello World!');
+    res.render('index');
 });
 
-app.listen(8080, function(){
-    console.log("Example app is listening to port 3000");
+app.get('/:timeInput', function(req, res){
+    var time;
+    if(/\d{8,}$/.test(req.params.timeInput)){
+        time = moment(req.params.timeInput, "X");
+    } else {
+        time = moment(req.params.timeInput, 'MMMM D, YYYY');
+    }
+    
+    if(time.isValid()){
+        res.json({
+            'date' : time.format('MMMM DD, YYYY'),
+            'unix' : time.format('X')
+        });
+    } else {
+        res.json({
+            'date' : null,
+            'unix' : null
+        });
+    }
+});
+
+app.listen(port, function(){
+    console.log("Listening on port: " + port);
 });
